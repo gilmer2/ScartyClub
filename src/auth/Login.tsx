4604c3f11208ef17609fcfa930559c39608b07ea
diego <diegoo05chable@gmail.com>
2024-06-12 08:22:41 -0600

    Cambios de login

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -2,18 +2,28 @@ import { useState } from "react";
 import "./Login.css";
 import { Button } from "../componets/Button";
 import { Input } from "../componets/Input";
+import { Link } from "react-router-dom";
+import { Paths } from "../path/paths";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Login = () => {
   return (
     <>
-    <br />
-    <br />
+      <br />
+      <br />
       <div className="login-form">
-        <h1>Login</h1>
+        <h1>Iniciar sesión</h1>
         <Input type="correo" placeholder="Correo electronico" />
         <Input type="password" placeholder="Contraseña" />
-        
-        <Button text="Iniciar sesión" onClick={() => {}} color="#007BFF" />
+        <Link className="form-text text-muted text-decoration-none" to="">
+          ¿Has olvidado tu contraseña?
+        </Link>
+        <Button
+          text="Iniciar sesión"
+          onClick={() => {}}
+          color="#007BFF"
+          type="submit"
+        />
       </div>
     </>
   );
--- src/componets/Button.tsx
+++ src/componets/Button.tsx
@@ -2,14 +2,16 @@ interface ButtonProps {
     text: string;
     onClick: () => void;
     color: string;
+    type: "button" | "submit" | "reset" | undefined;
 }
 
 export const Button: React.FC<ButtonProps> = ({
     text,
     onClick,
     color,
+    type,
 }) => {
     return (
-        <button style={{background: color}} onClick={onClick} className="button">{text}</button>
+        <button style={{background: color}} onClick={onClick} className="button" type={type}>{text}</button>
     );
 }
