ac88309ac7a82dc6af392326084da9216f5ea646
diego <diegoo05chable@gmail.com>
2024-06-12 08:38:04 -0600

    Cambios de login

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -2,7 +2,7 @@ import "./Login.css";
 import { Button } from "../componets/Button";
 import { Input } from "../componets/Input";
 import { Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Login = () => {
   return (
@@ -16,12 +16,25 @@ export const Login = () => {
         <Link className="form-text text-muted text-decoration-none" to="">
           ¿Has olvidado tu contraseña?
         </Link>
+        <>
+          <br />
+          <br />
+        </>
         <Button
           text="Iniciar sesión"
           onClick={() => {}}
           color="#007BFF"
           type="submit"
         />
+        <hr className="my-4"></hr>
+        <div className="d-flex flex-column">
+          <button className="btn btn-danger btn-user btn-block mb-2">
+            <i className="fab fa-google fa-fw"></i> Iniciar sesión con Google
+          </button>
+          <button className="btn btn-primary btn-user btn-block">
+            <i className="fab fa-facebook-f fa-fw"></i> Iniciar sesión con Facebook
+          </button>
+        </div>
       </div>
     </>
   );
