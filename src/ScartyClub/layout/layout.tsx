4a5a56a637d743470c90662faa5ed30956d7cc13
innovaforce_global <innovaforce_global@innovaforce.com.mx>
2024-06-19 07:34:46 -0600

    navbar

--- src/ScartyClub/layout/layout.tsx
+++ src/ScartyClub/layout/layout.tsx
@@ -1,31 +1,10 @@
-import { NavLink, Outlet } from "react-router-dom";
-import { Paths } from "../../path/paths";
+import { Outlet } from "react-router-dom";
+import { Navbar } from "./Navbar";
 
 export const Layout: React.FC = () => {
   return (
     <div>
-      <header className="App-header">
-        <h1>ScartyClub</h1>
-      </header>
-      <nav className="App-nav">
-        <ul>
-          <li>
-            <NavLink to="/">Inicio</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">Acerca de</NavLink>
-          </li>
-          <li>
-            <NavLink to="/services">Servicios</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contacto</NavLink>
-          </li>
-          <li>
-            <NavLink to={Paths.login}>Cerrar Sesion</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
       <div>
          <Outlet />
       </div>
